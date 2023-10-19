15db1ed48897744cbfb50a676e97b712335b21c2
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-19 13:28:01 -0400

    wip

--- main.ts
+++ main.ts
@@ -2,7 +2,27 @@
  *
  * Created by: Angelo Yalung
  * Created on: Oct 2023
- * This program ...
+ * This program chooses numbers between 0 and 99 and then shows what number is bigger than the other when shaking.
 */
 
-basic.showString('Hello, World!')
+// variable gets their number
+let num1: number = randint(0,99)
+let num2: number = randint(0,99)
+
+// if one number is bigger than why the other
+input.onGesture(Gesture.Shake, function() {
+  if (num1 > num2)
+    basic.showNumber(num1)
+    basic.showString(">")
+    basic.showNumber(num2)
+})
+
+// displaying number 1
+input.onButtonPressed(Button.A, function() {
+  basic.showNumber(num1)
+})
+
+// displaying number 2
+input.onButtonPressed(Button.B, function() {
+  basic.showNumber(num2)
+})
