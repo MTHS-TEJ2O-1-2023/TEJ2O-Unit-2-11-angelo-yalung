9bd806984349ebb6eeb70f8a08603e0e8093d312
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-24 13:11:59 -0400

    sdf

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@
  * This program chooses numbers between 0 and 99 and then shows what number is bigger than the other when shaking.
 */
 
+// variables
+let num1: number
+let num2: number
+
 // starting up
 basic.clearScreen()
 basic.pause(1000)
 basic.showIcon(IconNames.Happy)
-
-// variables
-let num1: number = randint(0, 99)
-let num2: number = randint(0, 99)
 num2 = randint(0, 99)
 num1 = randint(0, 99)
 
@@ -22,18 +22,18 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(num1)
     basic.showString('>')
     basic.showNumber(num2)
-    basic.showIcon(IconNames.Sad)
   }
   else {
     basic.showNumber(num1)
     basic.showString('<')
     basic.showNumber(num2)
-    basic.showIcon(IconNames.Sad)
   }
+  basic.showIcon(IconNames.Sad)
 })
 
 // displaying number 1
 input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
   basic.showString('#1')
   basic.showNumber(num1)
   basic.showIcon(IconNames.Happy)
@@ -41,6 +41,7 @@ input.onButtonPressed(Button.A, function () {
 
 // displaying number 2
 input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
   basic.showString('#2')
   basic.showNumber(num2)
   basic.showIcon(IconNames.Happy)
